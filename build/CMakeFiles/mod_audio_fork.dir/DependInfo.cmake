5d64ab2c6b5f9b90b4ea0fe7bbeef05eaf776932
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2025-09-10 14:34:32 +0700

    commit
    commit

---
+++ build/CMakeFiles/mod_audio_fork.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/usr/src/freeswitch/src/mod/applications/mod_audio_fork/mod_audio_fork.c" "CMakeFiles/mod_audio_fork.dir/mod_audio_fork.c.o" "gcc" "CMakeFiles/mod_audio_fork.dir/mod_audio_fork.c.o.d"
+  "/usr/src/freeswitch/src/mod/applications/mod_audio_fork/audio_pipe.cpp" "CMakeFiles/mod_audio_fork.dir/audio_pipe.cpp.o" "gcc" "CMakeFiles/mod_audio_fork.dir/audio_pipe.cpp.o.d"
+  "/usr/src/freeswitch/src/mod/applications/mod_audio_fork/lws_glue.cpp" "CMakeFiles/mod_audio_fork.dir/lws_glue.cpp.o" "gcc" "CMakeFiles/mod_audio_fork.dir/lws_glue.cpp.o.d"
+  "/usr/src/freeswitch/src/mod/applications/mod_audio_fork/parser.cpp" "CMakeFiles/mod_audio_fork.dir/parser.cpp.o" "gcc" "CMakeFiles/mod_audio_fork.dir/parser.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mod_audio_fork.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mod_audio_fork.dir/audio_pipe.cpp.o"
+  "CMakeFiles/mod_audio_fork.dir/audio_pipe.cpp.o.d"
+  "CMakeFiles/mod_audio_fork.dir/lws_glue.cpp.o"
+  "CMakeFiles/mod_audio_fork.dir/lws_glue.cpp.o.d"
+  "CMakeFiles/mod_audio_fork.dir/mod_audio_fork.c.o"
+  "CMakeFiles/mod_audio_fork.dir/mod_audio_fork.c.o.d"
+  "CMakeFiles/mod_audio_fork.dir/parser.cpp.o"
+  "CMakeFiles/mod_audio_fork.dir/parser.cpp.o.d"
+  "mod_audio_fork.pdb"
+  "mod_audio_fork.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/mod_audio_fork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
