899c8ef743ee5462c40ee312fcfe11df4d785643
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2025-09-15 08:36:37 +0700

    Update build_audio_fork.sh

--- build_audio_fork.sh
+++ build_audio_fork.sh
@@ -1,6 +1,17 @@
-apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev libwebsockets-dev
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make
-make install
+#!/bin/bash
 
+# Cập nhật danh sách gói
+apt-get update
+
+# Cài các thư viện cần thiết
+apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev libwebsockets-dev
+
+# Tạo thư mục build và vào đó
+mkdir -p build && cd build
+
+# Build với cmake
+cmake -DCMAKE_BUILD_TYPE=Release ..
+
+# Biên dịch và cài đặt
+make -j$(nproc)
+make install
